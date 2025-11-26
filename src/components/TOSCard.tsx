6146702b7002514f65b1c5914e52ee676cca107d
stanReynara Linux <stanleyreynara@gmail.com>
2025-11-26 15:36:59 +0700

    feature: accordion now not mutually exclusive

--- src/components/TOSCard.tsx
+++ src/components/TOSCard.tsx
@@ -10,27 +10,29 @@ type accordionDetails = {
 
 export default async function TOSCard({ title, accordionDetails }: Props) {
   return (
-    <>
-      <div className="card w-3xl bg-base-100 shadow-xl">
-        <div className="card-body border-2 border-primary rounded-xl">
-          <h1 className="card-title text-neutral text-3xl">{title}</h1>
-          <div className="divider divider-neutral"></div>
-          {accordionDetails.map((detail, index) => (
-            <div
-              key={index}
-              className="collapse collapse-arrow bg-primary/50 border-2 border-primary"
-            >
-              <input type="radio" name="my-accordion-2" defaultChecked />
-              <div className="collapse-title text-neutral font-semibold">
-                ✦ {detail.accordionTitle}
-              </div>
-              <div className="collapse-content text-neutral text-sm">
-                {detail.accordionContent}
-              </div>
+    <div className="card w-3xl bg-base-100 shadow-xl">
+      <div className="card-body border-2 border-primary rounded-xl">
+        <h1 className="card-title text-neutral text-3xl">{title}</h1>
+        <div className="divider divider-neutral"></div>
+
+        {accordionDetails.map((detail, index) => (
+          <div
+            key={index}
+            className="collapse collapse-arrow bg-primary/50 border-2 border-primary"
+          >
+            {/* Use checkbox instead of radio */}
+            <input type="checkbox" />
+
+            <div className="collapse-title text-neutral font-semibold">
+              ✦ {detail.accordionTitle}
             </div>
-          ))}
-        </div>
+
+            <div className="collapse-content text-neutral text-sm">
+              {detail.accordionContent}
+            </div>
+          </div>
+        ))}
       </div>
-    </>
+    </div>
   );
 }
